b039e109911f2932b4a37d3c215b296d8d7880a9
Huang Wei <imonyse@gmail.com>
2011-09-12 11:36:21 +0800

    change comment box layout

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110828114140) do
+ActiveRecord::Schema.define(:version => 20110720123751) do
 
   create_table "authentications", :force => true do |t|
     t.string   "provider"
@@ -40,13 +41,6 @@ ActiveRecord::Schema.define(:version => 20110828114140) do
     t.datetime "updated_at"
     t.integer  "access",            :default => 3
     t.integer  "score_requester",   :default => 0
-  end
-
-  create_table "notices", :force => true do |t|
-    t.text     "body"
-    t.string   "lang"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "notifications", :force => true do |t|
