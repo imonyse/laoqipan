c2db85f6685f0a7d769cd0fa950d67d01f8dda23
Huang Wei <imonyse@gmail.com>
2011-10-09 23:04:33 +0800

    use god monit juggernaut

--- config/god.rb
+++ config/god.rb
@@ -76,3 +76,41 @@ God.watch do |w|
     end
   end  
 end
+
+God.watch do |w|
+  w.name = 'juggernaut-worker'
+  w.interval = 30.seconds
+  w.env = {"RAILS_ENV" => "production"}
+  w.start = "juggernaut"
+  w.log = "#{RAILS_ROOT}/log/juggernaut-worker.log"
+  
+  w.start_if do |start|
+    start.condition(:process_running) do |c|
+      c.running = false
+    end
+  end
+  
+  w.restart_if do |restart|
+    restart.condition(:memory_usage) do |c|
+      c.above = 100.megabytes
+      c.times = [3, 5]
+    end
+    
+    restart.condition(:cpu_usage) do |c|
+      c.above = 50.percent
+      c.times = 5
+    end
+  end
+  
+  w.lifecycle do |on|
+    on.condition(:flapping) do |c|
+      c.to_state = [:start, :restart]
+      c.times = 5
+      c.within = 5.minute
+      c.transition = :unmonitored
+      c.retry_in = 10.minutes
+      c.retry_times = 5
+      c.retry_within = 2.hours
+    end
+  end  
+end
