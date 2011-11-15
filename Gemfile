d9fd5e9da4e04d5b88fb083577fc5d350e4d0cb0
Huang Wei <imonyse@gmail.com>
2011-11-15 15:00:35 +0800

    synatx highlighting for markdown code

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'juggernaut', '2.0.4'
 gem 'rake', '0.9.2'
 gem 'oily_png'
 gem 'redcarpet'
+gem 'coderay'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/helpers/broadcasts_helper.rb
+++ app/helpers/broadcasts_helper.rb
@@ -1,6 +1,12 @@
 module BroadcastsHelper
   def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :gh_blockcode, :fenced_code]
-    Redcarpet.new(text, *options).to_html.html_safe
+    syntax_on(Redcarpet.new(text, *options).to_html).html_safe
+  end
+  
+  def syntax_on(html)
+    html.gsub(/\<pre( lang="(.+?)")?\>\<code\>(.+?)\<\/code\>\<\/pre\>/m) do
+          CodeRay.scan($3, $2).div(:css => :class)
+    end
   end
 end
