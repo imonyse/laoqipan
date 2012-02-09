8ec766bbf97784125da36eb9cc7ae45665a7199d
Huang Wei <imonyse@gmail.com>
2012-02-09 17:11:01 +0800

    qualification is back

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -216,10 +216,9 @@ class GamesController < ApplicationController
   end
   
   def qualified?
-    return true
-    # if current_user.level == 0
-    #   redirect_to challenge_url, :alert => I18n.t(:need_qualification)
-    # end
+    if current_user.level == 0
+      redirect_to challenge_url, :alert => I18n.t(:need_qualification)
+    end
   end
   
 end
